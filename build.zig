cec53df99bab6cb4ae19046c3fd388d9de3c6407
devlopoer <devlopoer@gmail.com>
2026-07-15 15:04:46 +0000

    build: setup sdl build

---
+++ build.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "space-invader",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .link_libc = true,
+        }),
+    });
+    
+    exe.root_module.linkSystemLibrary("SDL2", .{});
+    b.installArtifact(exe);
+    
+    // Setup run command
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
